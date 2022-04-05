9121e35166f90334595276976c395b77aea8b4bf
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-04-05 15:04:23 +0100

    setting up feeds

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,9 +1,9 @@
 
 const HeaderItem = ({ Icon, title }) => {
     return (
-        <div>
-            <Icon className='h-8 mb-1'/>
-            <p>{title}</p>
+        <div className='flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-green-200 active:text-green-400'>
+            <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
+            <p className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</p>
         </div>
     )
 }
