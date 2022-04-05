120582ac7d2beb3b6359134bb1a03ef3ab3efa47
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-04-05 15:50:27 +0100

    I'm stuck

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Feeds from '../components/Feeds'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div >
       <Head>
@@ -27,4 +29,16 @@ export default function Home() {
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  // const allResults = JSON.stringify(results)
+  return {
+    props: {
+      results: request.results,
+    }
+  }
 }
